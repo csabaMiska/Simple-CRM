cef9541911c8d9227e6b9d8eac32cbb71c7ab809
unknown <miskacs90@icloud.com>
2025-02-28 22:31:52 +0100

    Dashboard and User Component added and Routing coded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
+    RouterModule,
     MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
@@ -22,5 +25,5 @@ import { MatListModule } from '@angular/material/list';
 })
 export class AppComponent {
   title = 'simple-crm';
-  showFiller = false;
+
 }
