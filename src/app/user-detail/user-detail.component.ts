7e088f3e2d4b1f5ab45b4807c4a461efd0e55eef
MiskaCs90 <miskacs90@icloud.com>
2025-03-02 14:17:12 +0100

    User Detail designed

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
